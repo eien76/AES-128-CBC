c7d113a2d243342e10e2a39c13f1e958c3e95993
eien76 <120074559+eien76@users.noreply.github.com>
2023-02-19 20:54:43 +0300

    Update Main.java

--- ISProject/src/Main.java
+++ ISProject/src/Main.java
@@ -25,6 +25,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.io.UnsupportedEncodingException;
 import java.security.spec.KeySpec;
 import java.util.Base64;
 import java.util.Scanner;
